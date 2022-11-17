026ead815226455ed64e93e46d7fadab46968708
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2022-11-16 23:01:07 -0500

    Added console log with string wooh

--- components/date.js
+++ components/date.js
@@ -2,5 +2,6 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
     const date = parseISO(dateString);
+    console.log("WOOH!")
     return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
